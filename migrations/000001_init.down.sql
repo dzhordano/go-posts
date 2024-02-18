562ee86b43f832915d8e23b6f506512e2a0b2287
dzhordano <dzhalal.2017@mail.ru>
2024-02-18 21:52:08 +0300

    added sql tables v1

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,3 +1,15 @@
+DROP TABLE IF EXISTS post_comments;
+
+DROP TABLE IF EXISTS users_posts;
+
+DROP TABLE IF EXISTS users_comments;
+
+DROP TABLE IF EXISTS comments;
+
+DROP TABLE IF EXISTS posts;
+
+DROP TABLE IF EXISTS admins;
+
 DROP TABLE IF EXISTS users;
 
 DROP TYPE IF EXISTS verification_type;
